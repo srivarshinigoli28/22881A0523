399372c47ffe52e41637b5d7f73787c87d6ca1d9
srivarshinigoli28 <golisrivarshini22cs@student.vardhaman.org>
2025-07-28 19:07:04 +0530

    Update Log.js

--- Log.js
+++ Log.js
@@ -1,10 +1,11 @@
 
 import fetch from 'node-fetch';
+import dotenv from 'dotenv';
 
-const client_id="a64a1caa-ffd5-461a-ac26-8102ab076265";
-const client_secret='adgxgwXXQGHpwteX';
-const access_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJnb2xpc3JpdmFyc2hpbmkyOEBnbWFpbC5jb20iLCJleHAiOjE3NTM2ODAxODIsImlhdCI6MTc1MzY3OTI4MiwiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6ImM1ZWUyYWE2LWZmZTMtNGU5Ny04YjBlLTBjODMyNWQ4ZDQ5OCIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6ImdvbGkgc3JpIHZhcnNoaW5pIiwic3ViIjoiYTY0YTFjYWEtZmZkNS00NjFhLWFjMjYtODEwMmFiMDc2MjY1In0sImVtYWlsIjoiZ29saXNyaXZhcnNoaW5pMjhAZ21haWwuY29tIiwibmFtZSI6ImdvbGkgc3JpIHZhcnNoaW5pIiwicm9sbE5vIjoiMjI4ODFhMDUyMyIsImFjY2Vzc0NvZGUiOiJ3UEVmR1oiLCJjbGllbnRJRCI6ImE2NGExY2FhLWZmZDUtNDYxYS1hYzI2LTgxMDJhYjA3NjI2NSIsImNsaWVudFNlY3JldCI6ImFkZ3hnd1hYUUdIcHd0ZVgifQ.THYZ7s26AvvCvExoG2gFBuSy2vvBnxVBX7ODTXSKBYE";
-
+dotenv.config();
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const access_token = process.env.ACCESS_TOKEN;
 const LOGGING_API_URL="http://20.244.56.144/evaluation-service/logs";
 
 export async function Log(service, level, source, message) {
